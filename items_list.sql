d41157ce112be23c32e5b70236adf8a69257614e
ZYRACX <soumya.lodh206@gmail.com>
2024-07-06 13:33:14 +0530

    Remove DEFAULT_ITEMS.json file Simplify README.md content Added .env.example file Added connect.ts file Added item_list.sql file

---
+++ items_list.sql
@@ -0,0 +1,74 @@
+    -- items can be obtained from mine.
+    INSERT INTO `items` (  `item_name`) VALUES ( 'Stone');
+    INSERT INTO `items` (  `item_name`) VALUES ( 'Iron');
+    INSERT INTO `items` (  `item_name`) VALUES ( 'Copper');
+    INSERT INTO `items` (  `item_name`) VALUES ( 'Silver');
+    INSERT INTO `items` (  `item_name`) VALUES ( 'Gold');
+    INSERT INTO `items` (  `item_name`) VALUES ( 'Platinum');
+    INSERT INTO `items` (  `item_name`) VALUES (  'Diamond');
+    INSERT INTO `items` (  `item_name`) VALUES (  'Urenium');
+    INSERT INTO `items` (  `item_name`) VALUES (  'Titanium');
+
+    -- items can be obtained from farming.
+    INSERT INTO `items` (  `item_name`) VALUES (  'Wheat');
+    INSERT INTO `items` (  `item_name`) VALUES (  'Rice');
+    INSERT INTO `items` (  `item_name`) VALUES (  'BlueBerry');
+    INSERT INTO `items` (  `item_name`) VALUES (  'Melon');
+
+
+    -- items can be obtained from journey.
+
+    -- woods
+    INSERT INTO `items` (  `item_name`) VALUES (  'Oak');
+    INSERT INTO `items` (  `item_name`) VALUES (  'Maple');
+    INSERT INTO `items` (  `item_name`) VALUES (  'Teak');
+    INSERT INTO `items` (  `item_name`) VALUES (  'Birch');
+    -- rare ones in woods
+    INSERT INTO `items` (  `item_name`) VALUES (  'Pine');
+    INSERT INTO `items` (  `item_name`) VALUES (  'Cherry');
+
+
+    -- electronics
+    INSERT INTO `items` (`item_name`) VALUES ('Circuit Board');
+    INSERT INTO `items` (`item_name`) VALUES ('Wire');
+    INSERT INTO `items` (`item_name`) VALUES ('Batteries');
+    INSERT INTO `items` (`item_name`) VALUES ('Connectors');
+    INSERT INTO `items` (`item_name`) VALUES ('Capacitors');
+    INSERT INTO `items` (`item_name`) VALUES ('Transistors');
+    -- rare ones in electronics
+    INSERT INTO `items` (`item_name`) VALUES ('Microcontroller');
+    INSERT INTO `items` (`item_name`) VALUES ('Sensors');
+    INSERT INTO `items` (`item_name`) VALUES ('Proccessor');
+    INSERT INTO `items` (`item_name`) VALUES ('Graphics Card');
+    INSERT INTO `items` (`item_name`) VALUES ('Sensors');
+    INSERT INTO `items` (`item_name`) VALUES ('Memory Chips');
+    INSERT INTO `items` (`item_name`) VALUES ('Switches');
+
+
+
+
+    -- items can be obtained from fishing.
+    INSERT INTO `items` (  `item_name`) VALUES (  'Salmon');
+    INSERT INTO `items` (  `item_name`) VALUES (  'Snapper');
+    INSERT INTO `items` (  `item_name`) VALUES (  'Perch');
+    INSERT INTO `items` (  `item_name`) VALUES (  'Cod');
+
+
+    -- items can be obtained from hunting. 
+    INSERT INTO `items` (  `item_name`) VALUES (  'Rabbit');
+    INSERT INTO `items` (  `item_name`) VALUES (  'Deer');
+    INSERT INTO `items` (  `item_name`) VALUES (  'Bear');
+    INSERT INTO `items` (  `item_name`) VALUES (  'Duck');
+    INSERT INTO `items` (  `item_name`) VALUES (  'Turkey');
+    INSERT INTO `items` (  `item_name`) VALUES (  'Wild boar');
+
+
+
+
+
+    -- Rare Very rare Items can be found any tasks
+    INSERT INTO `items` (  `item_name`) VALUES (  'Dragon Scale');
+    INSERT INTO `items` (  `item_name`) VALUES (  'Dragon Claw');
+    INSERT INTO `items` (  `item_name`) VALUES (  'Treasure');
+    INSERT INTO `items` (  `item_name`) VALUES (  'Manuscript');
+    INSERT INTO `items` (  `item_name`) VALUES (  'Fossil');
---
+++ utils/mysql/connect.ts
@@ -0,0 +1,8 @@
+import mysql from "mysql2/promise.js";
+
+export const pool = mysql.createPool({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+});
