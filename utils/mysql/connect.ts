7ac094ff24b567222c51290f967e4ac87749b34a
ZYRACX <soumya.lodh206@gmail.com>
2024-07-17 16:28:08 +0530

    "Remove useThemeSwicher hook and mysql connect utility"

--- utils/mysql/connect.ts
+++
@@ -1,8 +0,0 @@
-import mysql from "mysql2/promise.js";
-
-export const pool = mysql.createPool({
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
-});
